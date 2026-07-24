c0270ff173c316fae5d3208cf7107c9e673ff714
Richard de Vries <Pruyque@outlook.com>
2026-07-24 23:15:14 +0200

    Screaming!

---
+++ Assets/Scripts/LightSpotter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class LightSpotter : Spotter
+{
+    public static List<Spotter> Spotters = new List<Spotter>();
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+    }
+    private Light light;
+
+
+    private int frame = 0;
+    private void Update()
+    {
+        frame++;
+        // Every so often
+        if (frame % 10 == 4)
+        {
+            // Disable lights far away
+            light.enabled = Vector3.Distance(player.position, transform.position) < 40;
+        }
+    }
+
+    private void OnEnable()
+    {
+        Spotters.Add(this);
+        player = FindAnyObjectByType<PlayerMovement>().transform;
+    }
+
+    private void OnDisable()
+    {
+        Spotters.Remove(this);
+    }
+
+}
---
+++ Assets/Scripts/ScreamSpotter.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class ScreamSpotter : Spotter
+{
+    [SerializeField] private GameObject screamObject;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Transform player = Object.FindAnyObjectByType<PlayerMovement>().transform;
+
+        if (IsPointInCone(player.position))
+        {
+            Debug.Log("AAAAAHHHHHH!");
+            screamObject.SetActive(true);
+        }
+        else
+        {
+            screamObject.SetActive(false);
+        }
+    }
+
+    public static void SetEnabled(bool enabled)
+    {
+        foreach (ScreamSpotter screamer in FindObjectsByType<ScreamSpotter>())
+        {
+            screamer.enabled = enabled;
+        }
+
+    }
+}
